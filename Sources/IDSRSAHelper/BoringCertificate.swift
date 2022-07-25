3dc253f86a890c0d26a50592338f776979703455
Francesco Bianco <53116975+icesco@users.noreply.github.com>
2022-07-25 17:15:01 +0200

    Adds the ability to check if a certificate is expired or not

---
+++ Sources/IDSRSAHelper/BoringCertificate.swift
@@ -0,0 +1,84 @@
+//
+//  File.swift
+//  
+//
+//  Created by Francesco Bianco on 25/07/22.
+//
+
+import Foundation
+import openssl_grpc
+import Security
+
+public struct BoringCertificate {
+    
+    //https://stackoverflow.com/questions/8850524/seccertificateref-how-to-get-the-certificate-information/
+    
+    public static func expiryDate(_ certificateData: Data,
+                                  passPhrase: String,
+                                  completion: @escaping (Date?) -> Void) {
+        guard let bundle = CryptoBundle(from: certificateData, password: passPhrase) else {
+            completion(nil)
+            return
+        }
+        
+        var certData = SecCertificateCopyData(bundle.certificate) as Data
+        
+        certData.withUnsafeBytes({ (bytes: UnsafePointer<UInt8>?) -> Void in
+                         //Use `bytes` inside this closure
+            var byte = bytes
+            guard let certificate = d2i_X509(nil, &byte , certificateData.count) else {
+                completion(nil)
+                return
+            }
+            completion(Self.getExpiryDate(certificateX509: certificate))
+        })
+   }
+    
+    
+    private static func getExpiryDate(certificateX509: OpaquePointer) -> Date? {
+        
+        guard let certificateExpiryASN1 = X509_get_notAfter(certificateX509) else {
+            return nil
+        }
+        guard let certificateExpiryASN1Generalized = ASN1_TIME_to_generalizedtime(certificateExpiryASN1, nil) else {
+            return nil
+        }
+        
+        guard let certificateExpiryData = ASN1_STRING_data(certificateExpiryASN1Generalized) else { return nil }
+        
+        // ASN1 generalized times look like this: "20131114230046Z"
+        //                                format:  YYYYMMDDHHMMSS
+        //                               indices:  01234567890123
+        //                                                   1111
+        // There are other formats (e.g. specifying partial seconds or
+        // time zones) but this is good enough for our purposes since
+        // we only use the date and not the time.
+        //
+        // (Source: http://www.obj-sys.com/asn1tutorial/node14.html)
+        
+        guard let expiryTimeStr = NSString(utf8String: certificateExpiryData) else {
+            return nil
+        }
+        var expiryDateComponents = DateComponents()
+        
+        
+        expiryDateComponents.year   = expiryTimeStr.substring(with: NSMakeRange(0, 4)).intValue ?? 0
+        expiryDateComponents.month  = expiryTimeStr.substring(with: NSMakeRange(4, 2)).intValue ?? 0
+        expiryDateComponents.day    = expiryTimeStr.substring(with: NSMakeRange(6, 2)).intValue ?? 0
+        expiryDateComponents.hour   = expiryTimeStr.substring(with: NSMakeRange(8, 2)).intValue ?? 0
+        expiryDateComponents.minute = expiryTimeStr.substring(with: NSMakeRange(10, 2)).intValue ?? 0
+        expiryDateComponents.second = expiryTimeStr.substring(with: NSMakeRange(12, 2)).intValue ?? 0
+        
+        let calendar = Calendar.current
+        return calendar.date(from: expiryDateComponents)
+    }
+    
+}
+
+extension String {
+    
+    var intValue: Int? {
+        return Int(self)
+    }
+    
+}
